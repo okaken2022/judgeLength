9abfbe093102d5d42a70e478b8d322855b86599c
okaken2022 <97785180+okaken2022@users.noreply.github.com>
2022-07-17 22:33:44 +0900

    三項演算子で記述

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 // 引数の文字数が5文字以上かつ10文字未満ならtrueを返す関数を作る
+// const judgeLength = (str) => {
+//   if(5 <= str.length && str.length < 10) {
+//     return true
+//   } else {
+//     return false
+//   }
+// };
+// console.log(judgeLength("apple"));
+
+// 三項演算子で記述
 const judgeLength = (str) => {
-  if(5 <= str.length && str.length < 10) {
-    return true
-  } else {
-    return false
-  }
+  return 5 <= str.length && str.length < 10 ? true : false;
 };
 console.log(judgeLength("apple"));
